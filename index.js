17c2257fefb155a26e7e174d89c804032cd94362
sergey.p <sergey.p@didww.com>
2018-08-29 23:51:21 +0300

    get request data

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+// Dependencies
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req,res) => {
+  //get url
+  const parsedUrl = url.parse(req.url, true);
+
+  //get pathname
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  //get method
+  const method = req.method.toLowerCase();
+
+  //get headers
+  const headers = req.headers;
+
+  //get query params
+  const queryParams = parsedUrl.query;
+
+  //send response
   res.end("Hello world\n");
+
+  //log the request
+  console.log(headers);
 });
 
 server.listen(3000, () => {
